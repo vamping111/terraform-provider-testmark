3ec4696ed67d24b6c7148b31c9fe91cf65be8825
Margarita Fedotova <mfedotova@croc.ru>
2023-09-25 01:20:27 +0300

    paas: add base types for service definition and schema building

---
+++ internal/service/paas/services/converter.go
@@ -0,0 +1,271 @@
+package services
+
+import (
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/paas"
+)
+
+// ExpandUsers converts terraform representation of list of users to api representation.
+func (s service) ExpandUsers(tfList []interface{}, forDatabase bool) []*paas.User {
+	if len(tfList) == 0 {
+		return nil
+	}
+
+	var users []*paas.User
+
+	for _, tfMapRaw := range tfList {
+		tfMap, ok := tfMapRaw.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		user := s.toInterface().ExpandUser(tfMap, forDatabase)
+		if user == nil {
+			continue
+		}
+
+		users = append(users, user)
+	}
+
+	return users
+}
+
+// ExpandUser converts terraform representation of service user to api representation.
+// If forDatabase is true, user is considered a database user.
+func (s service) ExpandUser(tfMap map[string]interface{}, forDatabase bool) *paas.User {
+	if tfMap == nil {
+		return nil
+	}
+
+	user := &paas.User{}
+
+	if v, ok := tfMap["name"].(string); ok && v != "" {
+		user.Name = aws.String(v)
+		delete(tfMap, "name")
+	}
+
+	if forDatabase {
+		user.Parameters = s.toInterface().expandDatabaseUserParameters(tfMap)
+	} else {
+		user.Parameters = s.toInterface().expandUserParameters(tfMap)
+	}
+
+	return user
+}
+
+// ExpandDatabases converts terraform representation of list of databases to api representation.
+func (s service) ExpandDatabases(tfList []interface{}) []*paas.Database {
+	if len(tfList) == 0 {
+		return nil
+	}
+
+	var databases []*paas.Database
+
+	for _, tfMapRaw := range tfList {
+		tfMap, ok := tfMapRaw.(map[string]interface{})
+		if !ok {
+			continue
+		}
+
+		database := s.toInterface().ExpandDatabase(tfMap)
+		if database == nil {
+			continue
+		}
+
+		databases = append(databases, database)
+	}
+
+	return databases
+}
+
+// ExpandDatabase converts terraform representation of database to api representation.
+func (s service) ExpandDatabase(tfParameters map[string]interface{}) *paas.Database {
+	if tfParameters == nil {
+		return nil
+	}
+
+	database := &paas.Database{}
+
+	if v, ok := tfParameters["backup_enabled"].(bool); ok {
+		database.BackupEnabled = aws.Bool(v)
+		delete(tfParameters, "backup_enabled")
+	}
+
+	if v, ok := tfParameters["name"].(string); ok && v != "" {
+		database.Name = aws.String(v)
+		delete(tfParameters, "name")
+	}
+
+	if v, ok := tfParameters["user"].([]interface{}); ok && len(v) > 0 {
+		database.Users = s.toInterface().ExpandUsers(v, true)
+		delete(tfParameters, "user")
+	}
+
+	database.Parameters = s.toInterface().expandDatabaseParameters(tfParameters)
+
+	return database
+}
+
+// expandUserParameters converts terraform representation of service-specific user parameters
+// to api representation.
+//
+// If PaaS service has specific user parameters, it should override this method.
+func (s service) expandUserParameters(_ map[string]interface{}) UserParameters {
+	return nil
+}
+
+// expandDatabaseParameters converts terraform representation of service-specific database parameters
+// to api representation.
+//
+// If PaaS service has specific database parameters, it should override this method.
+func (s service) expandDatabaseParameters(_ map[string]interface{}) DatabaseParameters {
+	return nil
+}
+
+// expandDatabaseUserParameters converts terraform representation of service-specific database user parameters
+// to api representation.
+//
+// If PaaS service has specific database user parameters, it should override this method.
+func (s service) expandDatabaseUserParameters(_ map[string]interface{}) DatabaseUserParameters {
+	return nil
+}
+
+// FlattenServiceParametersUsersDatabases converts all blocks of service-specific parameters
+// from api to terraform representation.
+//
+// It's Expand analogue is represented by three separate methods:
+// ExpandServiceParameters (overridden on service level), ExpandUsers and ExpandDatabases,
+// because these blocks are separated in api representation.
+func (s service) FlattenServiceParametersUsersDatabases(
+	serviceParameters ServiceParameters,
+	users []*paas.User,
+	databases []*paas.Database,
+) map[string]interface{} {
+	tfMap := s.toInterface().flattenServiceParameters(serviceParameters)
+
+	if s.usersEnabled {
+		tfMap["user"] = s.toInterface().FlattenUsers(users, false)
+	}
+
+	if s.databasesEnabled {
+		tfMap["database"] = s.toInterface().FlattenDatabases(databases)
+	}
+
+	return tfMap
+}
+
+// FlattenUsers converts api representation of list of users to terraform representation.
+func (s service) FlattenUsers(users []*paas.User, forDatabase bool) []interface{} {
+	if len(users) == 0 {
+		return nil
+	}
+
+	var tfList []interface{}
+
+	for _, user := range users {
+		if user == nil {
+			continue
+		}
+
+		tfList = append(tfList, s.toInterface().FlattenUser(user, forDatabase))
+	}
+
+	return tfList
+}
+
+// FlattenUser converts api representation of service user to terraform representation.
+// If forDatabase is true, user is considered a database user.
+func (s service) FlattenUser(user *paas.User, forDatabase bool) map[string]interface{} {
+	if user == nil {
+		return map[string]interface{}{}
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := user.Name; v != nil {
+		tfMap["name"] = v
+	}
+
+	var parameters map[string]interface{}
+	if forDatabase {
+		parameters = s.toInterface().flattenDatabaseUserParameters(user.Parameters)
+	} else {
+		parameters = s.toInterface().flattenUserParameters(user.Parameters)
+	}
+
+	for k, v := range parameters {
+		tfMap[k] = v
+	}
+
+	return tfMap
+}
+
+// FlattenDatabases converts api representation of list of databases to terraform representation.
+func (s service) FlattenDatabases(databases []*paas.Database) []interface{} {
+	if len(databases) == 0 {
+		return nil
+	}
+
+	var tfList []interface{}
+
+	for _, database := range databases {
+		if database == nil {
+			continue
+		}
+
+		tfList = append(tfList, s.toInterface().FlattenDatabase(database))
+	}
+
+	return tfList
+}
+
+// FlattenDatabase converts api representation of database to terraform representation.
+func (s service) FlattenDatabase(database *paas.Database) map[string]interface{} {
+	if database == nil {
+		return map[string]interface{}{}
+	}
+
+	tfMap := map[string]interface{}{}
+
+	if v := database.BackupEnabled; v != nil {
+		tfMap["backup_enabled"] = v
+	}
+
+	if v := database.Name; v != nil {
+		tfMap["name"] = v
+	}
+
+	if v := database.Users; v != nil {
+		tfMap["user"] = s.toInterface().FlattenUsers(database.Users, true)
+	}
+
+	for k, v := range s.toInterface().flattenDatabaseParameters(database.Parameters) {
+		tfMap[k] = v
+	}
+
+	return tfMap
+}
+
+// flattenUserParameters converts api representation of service-specific user parameters
+// to terraform representation.
+//
+// If PaaS service has specific user parameters, it should override this method.
+func (s service) flattenUserParameters(_ UserParameters) map[string]interface{} {
+	return nil
+}
+
+// flattenDatabaseParameters converts api representation of service-specific database parameters
+// to terraform representation.
+//
+// If PaaS service has specific database parameters, it should override this method.
+func (s service) flattenDatabaseParameters(_ DatabaseParameters) map[string]interface{} {
+	return nil
+}
+
+// flattenDatabaseUserParameters converts api representation of service-specific database user parameters
+// to terraform representation.
+//
+// If PaaS service has specific database user parameters, it should override this method.
+func (s service) flattenDatabaseUserParameters(_ DatabaseUserParameters) map[string]interface{} {
+	return nil
+}
---
+++ internal/service/paas/services/schema_builder.go
@@ -0,0 +1,272 @@
+package services
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+// ResourceSchema returns a full schema of service parameters for resource.
+//
+// It includes:
+//   - common `class` parameter that holds chosen class of service
+//   - service-specific parameters
+//   - user parameters if service can hold them
+//   - database parameters if service can hold them
+func (s service) ResourceSchema() *schema.Schema {
+	serviceSchema := map[string]*schema.Schema{
+		"class": {
+			Type:         schema.TypeString,
+			Optional:     true,
+			Default:      s.defaultClass,
+			ForceNew:     true,
+			ValidateFunc: validation.StringInSlice(s.class, false),
+		},
+	}
+
+	for k, v := range s.toInterface().serviceParametersSchema() {
+		serviceSchema[k] = v
+	}
+
+	if s.usersEnabled {
+		serviceSchema["user"] = s.userSchema(s.toInterface().userParametersSchema())
+	}
+
+	if s.databasesEnabled {
+		serviceSchema["database"] = s.databaseSchema(s.toInterface().databaseParametersSchema())
+	}
+
+	var requiredWith, conflictsWith []string
+	if s.dataVolumeRequired {
+		requiredWith = append(requiredWith, "data_volume")
+	}
+
+	if !s.allowArbitrator {
+		conflictsWith = append(conflictsWith, "arbitrator_required")
+	}
+
+	if !s.allowBackup {
+		conflictsWith = append(conflictsWith, "backup_settings")
+	}
+
+	return &schema.Schema{
+		Type:          schema.TypeList,
+		MaxItems:      1,
+		Optional:      true,
+		ForceNew:      true,
+		ExactlyOneOf:  ManagedServiceTypes(),
+		ConflictsWith: conflictsWith,
+		RequiredWith:  requiredWith,
+		Elem: &schema.Resource{
+			Schema: serviceSchema,
+		},
+	}
+}
+
+// userSchema returns a schema of user parameters for resource.
+//
+// It includes:
+//   - common parameter name that holds username
+//   - service-specific user parameters, if service has them
+func (s service) userSchema(userParametersSchema map[string]*schema.Schema) *schema.Schema {
+	userSchema := map[string]*schema.Schema{
+		"name": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+	}
+
+	for k, v := range userParametersSchema {
+		userSchema[k] = v
+	}
+
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: true,
+		MaxItems: 1000,
+		Elem: &schema.Resource{
+			Schema: userSchema,
+		},
+	}
+}
+
+// databaseSchema returns a schema of databse parameters for resource.
+//
+// It includes:
+//   - common `backup_enabled` and `name` parameters
+//   - service-specific database user parameters if service has them
+//   - service-specific database parameters, if service has them
+func (s service) databaseSchema(databaseParametersSchema map[string]*schema.Schema) *schema.Schema {
+	databaseSchema := map[string]*schema.Schema{
+		"backup_enabled": {
+			Type:     schema.TypeBool,
+			Optional: true,
+			Default:  false,
+		},
+		"name": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+	}
+
+	if s.usersEnabled {
+		databaseSchema["user"] = s.userSchema(s.toInterface().databaseUserParametersSchema())
+	}
+
+	for k, v := range databaseParametersSchema {
+		databaseSchema[k] = v
+	}
+
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: true,
+		MaxItems: 1000,
+		Elem: &schema.Resource{
+			Schema: databaseSchema,
+		},
+	}
+}
+
+// serviceParametersSchema represents resource schema for service-specific parameters except for databases and users.
+//
+// If PaaS service has specific parameters, it should override this method.
+func (s service) serviceParametersSchema() map[string]*schema.Schema {
+	return nil
+}
+
+// userParametersSchema represents resource schema for service-specific user parameters.
+//
+// If PaaS service has specific user parameters, it should override this method.
+func (s service) userParametersSchema() map[string]*schema.Schema {
+	return nil
+}
+
+// databaseParametersSchema represents resource schema for service-specific database parameters.
+//
+// If PaaS service has specific database parameters, it should override this method.
+func (s service) databaseParametersSchema() map[string]*schema.Schema {
+	return nil
+}
+
+// databaseUserParametersSchema represents resource schema for service-specific database user parameters.
+//
+// If PaaS service has specific database user parameters, it should override this method.
+func (s service) databaseUserParametersSchema() map[string]*schema.Schema {
+	return nil
+}
+
+// DataSourceSchema returns a full schema of service parameters for datasource.
+//
+// It includes the same blocks as ResourceSchema.
+func (s service) DataSourceSchema() *schema.Schema {
+	serviceSchema := map[string]*schema.Schema{
+		"class": {
+			Type:     schema.TypeString,
+			Computed: true,
+		},
+	}
+
+	for k, v := range s.toInterface().serviceParametersDataSourceSchema() {
+		serviceSchema[k] = v
+	}
+
+	if s.usersEnabled {
+		serviceSchema["user"] = s.userDataSourceSchema(s.toInterface().userParametersDataSourceSchema())
+	}
+
+	if s.databasesEnabled {
+		serviceSchema["database"] = s.databaseDataSourceSchema(s.toInterface().databaseParametersSchema())
+	}
+
+	return &schema.Schema{
+		Type:     schema.TypeList,
+		Computed: true,
+		Elem: &schema.Resource{
+			Schema: serviceSchema,
+		},
+	}
+}
+
+// userDataSourceSchema returns a schema of user parameters for datasource.
+//
+// It includes the same blocks as userSchema.
+func (s service) userDataSourceSchema(userParametersSchema map[string]*schema.Schema) *schema.Schema {
+	userSchema := map[string]*schema.Schema{
+		"name": {
+			Type:     schema.TypeString,
+			Computed: true,
+		},
+	}
+
+	for k, v := range userParametersSchema {
+		userSchema[k] = v
+	}
+
+	return &schema.Schema{
+		Type:     schema.TypeSet,
+		Computed: true,
+		Elem: &schema.Resource{
+			Schema: userSchema,
+		},
+	}
+}
+
+// databaseSchema returns a schema of databse parameters for resource.
+//
+// It includes the same blocks as databaseSchema.
+func (s service) databaseDataSourceSchema(databaseParametersSchema map[string]*schema.Schema) *schema.Schema {
+	databaseSchema := map[string]*schema.Schema{
+		"backup_enabled": {
+			Type:     schema.TypeBool,
+			Computed: true,
+		},
+		"name": {
+			Type:     schema.TypeString,
+			Computed: true,
+		},
+	}
+
+	if s.usersEnabled {
+		databaseSchema["user"] = s.userDataSourceSchema(s.toInterface().databaseUserParametersDataSourceSchema())
+	}
+
+	for k, v := range databaseParametersSchema {
+		databaseSchema[k] = v
+	}
+
+	return &schema.Schema{
+		Type:     schema.TypeSet,
+		Computed: true,
+		Elem: &schema.Resource{
+			Schema: databaseSchema,
+		},
+	}
+}
+
+// serviceParametersDataSourceSchema represents datasource schema for service-specific parameters
+// except for databases and users.
+//
+// If PaaS service has specific parameters, it should override this method.
+func (s service) serviceParametersDataSourceSchema() map[string]*schema.Schema {
+	return nil
+}
+
+// userParametersDataSourceSchema represents datasource schema for service-specific user parameters.
+//
+// If PaaS service has specific user parameters, it should override this method.
+func (s service) userParametersDataSourceSchema() map[string]*schema.Schema {
+	return nil
+}
+
+// databaseParametersDataSourceSchema represents datasource schema for service-specific database parameters.
+//
+// If PaaS service has specific database parameters, it should override this method.
+func (s service) databaseParametersDataSourceSchema() map[string]*schema.Schema {
+	return nil
+}
+
+// databaseUserParametersDataSourceSchema represents datasource schema for service-specific database user parameters.
+//
+// If PaaS service has specific database user parameters, it should override this method.
+func (s service) databaseUserParametersDataSourceSchema() map[string]*schema.Schema {
+	return nil
+}
